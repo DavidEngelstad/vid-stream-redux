f5435291703c26df02e9e72ae9551bd85d6ed314
David Engelstad <davidengelstad@gmail.com>
2019-02-18 13:47:41 -0800

    Initial Commit

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>Hello From App!</div>
+      </div>
+    );
+  }
+}
+
+export default App;
